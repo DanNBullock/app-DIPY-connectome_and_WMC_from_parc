78131dc3ee7f60a412de43f840425b57151f7224
Dan Bullock <bullo092@umn.edu>
2022-06-03 15:04:39 -0500

    tested and working

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Created on Thu Jun  2 17:13:31 2022
 import os
 import sys
 
-sys.path.append('wma_pyTools/')
+sys.path.append('wma_pyTools')
 startDir=os.getcwd()
 #some how set a path to wma pyTools repo directory
 #wmaToolsDir='wma_pyTools'
@@ -29,7 +29,7 @@ from dipy.tracking.utils import reduce_labels
 from dipy.tracking import utils
 
 #os.chdir(startDir)
-
+from scipy.io import savemat
 import os
 import json
 import numpy as np
@@ -41,13 +41,11 @@ import pandas as pd
 with open('config.json') as config_json:
 	config = json.load(config_json)
 
-outDir='output'
-if not os.path.exists(outDir):
-    os.makedirs(outDir)
-if not os.path.exists(os.path.join(outDir,'wmc')):
-    os.makedirs(os.path.join(outDir,'wmc'))
-if not os.path.exists(os.path.join(outDir,'connectome')):
-    os.makedirs(os.path.join(outDir,'connectome'))
+
+if not os.path.exists(os.path.join('wmc')):
+    os.makedirs(os.path.join('wmc'))
+if not os.path.exists(os.path.join('connectome')):
+    os.makedirs(os.path.join('connectome'))
     
 parcIn=nib.load(config['parc'])
 lookupTable=wmaPyTools.genUtils.parcJSON_to_LUT(config['label'])
@@ -65,7 +63,16 @@ M, grouping=utils.connectivity_matrix(orientedStreams, np.round(renumberedAtlasN
                         return_mapping=True,
                         mapping_as_streamlines=False)
 
-classification=wmaPyTools.streamlineTools.wmc_from_DIPY_connectome(grouping,lookupTable)
+#classification production
+classification=wmaPyTools.streamlineTools.wmc_from_DIPY_connectome(grouping,reducedLookupTable)
+#save it
+savemat(os.path.join('wmc','classification.mat'),{ "classification": {"names": classification['names'], "index": classification['index'] }})
 
-#what format is network output?
+#create the conmat
+wmaPyTools.genUtils.bl_conmat_fromDIPYandParc(M,reducedLookupTable,os.path.join('connectome'))
 
+#create the network output
+#conmat_to_JGFZ(arrayORcsv,indexIn,labelIn)
+#no, just use the app, this format is rediculous
+#https://brainlife.io/app/5f1fa182beafe9548f622a7e
+#wmaPyTools.genUtils.conmat_to_JGFZ(M,os.path.join('connectome','index.json'),os.path.join('connectome','label.json'))
